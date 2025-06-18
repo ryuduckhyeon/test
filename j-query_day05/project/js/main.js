ee686be3c674e8668f82e51379c3cf84ca4b2bd1
ryuduckhyeon <37653915+ryuduckhyeon@users.noreply.github.com>
2025-06-18 17:38:46 +0900

    Add files via upload

--- j-query_day05/project/js/main.js
+++ j-query_day05/project/js/main.js
@@ -3,7 +3,9 @@ $(function(){
         // slick 슬라이드 자동 실행
         autoplay: true,
         // fadeInFadeout 슬라이드
-        fade:true
+        fade:true,
+        // 페이지 버튼
+        dots:true
     });
 
 
