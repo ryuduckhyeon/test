872e7458f00a92e8ec62193af110368fce8770a7
ryuduckhyeon <37653915+ryuduckhyeon@users.noreply.github.com>
2025-06-18 17:43:45 +0900

    Add files via upload

--- j-query_day05/js/main.js
+++ j-query_day05/js/main.js
@@ -1,3 +1,17 @@
 $(function(){
-    
+    $('.slider').slick({
+        // slick 슬라이드 자동 실행
+        autoplay: true,
+        // fadeInFadeout 슬라이드
+        fade:true,
+        // 페이지 버튼
+        dots:true
+    });
+
+
+
+
+
+
+
 })
