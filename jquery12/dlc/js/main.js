941368f5289e4622ead622f85a3ede7e9297786b
ryuduckhyeon <37653915+ryuduckhyeon@users.noreply.github.com>
2025-06-24 17:34:41 +0900

    Add files via upload

---
+++ jquery12/dlc/js/main.js
@@ -0,0 +1,18 @@
+$(function
+    ()
+    {
+        // //2단메뉴 숨기기
+        // $(".dept2").hide();
+        $("ul.gnb").mouseenter(function(){
+            $("#header_wrap").stop().animate({'height' : '351px'}, 200);
+            //2단메뉴 보이게
+            $(".dept2").show();
+            $("#header_wrap").css({"border-bottom":none});
+        })
+        $("#header_wrap").mouseleave(function(){
+            $("#header_wrap").stop().animate({'height' : '70px'}, 200)
+            //2단메뉴 숨기기
+            $(".dept2").hide();
+            $("#header_wrap").css({"border-bottom":"1px solid #c4c4c4"});
+        })
+    })
