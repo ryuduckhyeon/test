4bbb36f2175e3f780c2a3e282c1c48ecf54d0d9a
ryuduckhyeon <37653915+ryuduckhyeon@users.noreply.github.com>
2025-06-24 16:15:38 +0900

    Add files via upload

--- jquery12/js/index.js
+++ jquery12/js/index.js
@@ -7,10 +7,12 @@ $(function
             $("#header_wrap").stop().animate({'height' : '380px'});
             //2단메뉴 보이게
             $(".dept2").show();
+            $("#header_wrap").css({"border-bottom":none});
         })
         $("#header_wrap").mouseleave(function(){
             $("#header_wrap").stop().animate({'height' : '110px'})
             //2단메뉴 숨기기
             $(".dept2").hide();
+            $("#header_wrap").css({"border-bottom":"1px solid #c4c4c4"});
         })
     })
